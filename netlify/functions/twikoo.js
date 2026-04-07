6fc54d2467e7faa429d5f683643be6f99cc7f03f
wxiao2 <42410976+xitcz@users.noreply.github.com>
2026-04-07 21:36:39 +0800

    Update twikoo.js

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,23 @@
-exports.handler = require('twikoo-netlify').handler
+const { handler: twikooHandler } = require('twikoo-netlify');
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+};
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers: corsHeaders, body: '' };
+  }
+
+  const response = await twikooHandler(event, context);
+
+  return {
+    ...response,
+    headers: {
+      ... (response.headers || {}),
+      ...corsHeaders,
+    },
+  };
+};
